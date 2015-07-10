061b487f0c9a3715eb6b690e9aea531e337be0b6
Alex Carlin <carlin@ucdavis.edu>
2015-07-10 15:16:45 -0700

    Updated run_enzdes.bash

--- run_enzdes.bash
+++ run_enzdes.bash
@@ -5,13 +5,12 @@
 #$ -o logs
 #$ -N enzdes 
 
-
 #example UM_100_H101W129_1_1lua_SBO_HW_HW_1.pdb
 
 export ROSETTA3_DB=/share/tmp-data-1/siegellab/Rosetta/main/database/
 export PATH=/share/tmp-data-1/siegellab/Rosetta/main/source/bin/:$PATH
 
-i=$( awk 'NR=="'${SGE_TASK_ID}'" { print $0 }' enzdes_list.txt )
+i=$( awk 'NR=="'${SGE_TASK_ID}'" { print $0 }' match_out_list.txt )
 
 IFS='_' read -a array <<< "$i" 
 
@@ -22,5 +21,5 @@ DYAD=$( printf "%s_%s" ${array[6]} ${array[7]} )
 echo 'REMARK 666 MATCH TEMPLATE X' $LIGAND '   0 MATCH MOTIF B NAX    1  3  1 ' > enzdes_in/$i.nad.pdb
 cat match_out/$i >> enzdes_in/$i.nad.pdb
 
-rosetta_scripts.linuxgccrelease @ enzdes.flags -s enzdes_in/$i.nad.pdb -extra_res_fa input_params/${SCAFFOLD}.params -extra_res_fa params/${LIGAND}.params -out:path:all enzdes_out_big -enzdes:cstfile cst/enzdes/NAD_${LIGAND}_${DYAD}.cst 
+rosetta_scripts.linuxgccrelease @ enzdes.flags -s enzdes_in/$i.nad.pdb -extra_res_fa input_params/${SCAFFOLD}.params -extra_res_fa params/${LIGAND}.params -out:path:all enzdes_out -enzdes:cstfile cst/enzdes/${LIGAND}_${DYAD}_NAD.cst 
 
