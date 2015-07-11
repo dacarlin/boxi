53773cb1f46d0da67db098baed86cd9da356a0fe
Alex Carlin <carlin@ucdavis.edu>
2015-07-10 21:22:49 -0700

    Fixed secondary matcher

--- run_secondary_matcher.bash
+++ run_secondary_matcher.bash
@@ -13,4 +13,11 @@ s=$( awk 'NR=="'${SGE_TASK_ID}'" { print $1 }' scaffold_list.txt )
 for i in RBO SBO OXI; do 
   for j in HW_HW HW_YTS YTS_YTS; do 
 
-echo match.linuxgccrelease -s input_pdb/$s.pdb -match:geometric_constraint_file cst/secondary_matcher/${i}_${j}.cst -extra_res_fa params/${i}.params input_params/${s}.params -match:lig_name $i -match:scaffold_active_site_residues input_pos/$s.pos -ex1 -ex2 -ex1aro -ex2aro -output_format PDB -consolidate_matches -out:path:all match_out -ignore_unrecognized_res 1 -match_grouper SameSequenceGrouper
+match.linuxgccrelease -s input_pdb/$s.pdb -match:geometric_constraint_file cst/secondary_matcher/${i}_${j}.cst -extra_res_fa params/${i}.params input_params/${s}.params -match:lig_name $i -match:scaffold_active_site_residues input_pos/$s.pos -ex1 -ex2 -ex1aro -ex2aro -output_format PDB -consolidate_matches -out:path:all match_out -ignore_unrecognized_res 1 -match_grouper SameSequenceGrouper
+
+
+mv UM_* secondary_match_out/
+
+
+done 
+done
