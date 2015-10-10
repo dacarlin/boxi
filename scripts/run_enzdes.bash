a090f10bc97c5a6721a01608e19588f00d0e282b
Alex Carlin <carlin@ucdavis.edu>
2015-10-10 15:46:14 -0700

    Fixed enzdes submit script

--- scripts/run_enzdes.bash
+++ scripts/run_enzdes.bash
@@ -1,25 +1,21 @@
-#!/bin/bash
 #$ -S /bin/bash
 #$ -e /home/carlin/boxi/logs
 #$ -o /home/carlin/boxi/logs
-#$ -N enzdes 
+#$ -N design 
 
 #example UM_100_H101W129_1_1lua_SBO_HW_HW_1.pdb
 
 export ROSETTA3_DB=/share/tmp-data-1/siegellab/Rosetta/main/database/
 export PATH=/share/tmp-data-1/siegellab/Rosetta/main/source/bin/:$PATH
-cd /home/carlin/boxi 
+cd /home/carlin/boxi
 
-LINE="$( awk 'NR=="'${SGE_TASK_ID}'" { print $0 }' list/match_out.txt )"
-IFS='_' read -a A <<< "$LINE" 
+S=$( sed "${SGE_TASK_ID}q;d" list/design_in.txt ) 
+LIG=$( echo $S | cut -d_ -f6 ) 
+PDB=$( echo $S | cut -d_ -f5 ) 
+CST=$( echo $S | cut -d_ -f6,7,8 ) 
 
-SCAFFOLD=${A[4]} 
-LIGAND=${A[5]}
-DYAD=$( printf "%s_%s" ${A[6]} ${A[7]} )
+echo 'REMARK 666 MATCH TEMPLATE X' $LIG '0 MATCH MOTIF B NAX 1 3 1' > out/enzdes/$S 
+cat out/secondary/$S >> out/enzdes/$S 
 
-( echo 'REMARK 666 MATCH TEMPLATE X ' ${A[5]} ' 0 MATCH MOTIF B NAX 1 3 1' && cat $LINE ) > ${LINE/.pdb/_nad.pdb} 
-
-#head ${LINE/.pdb/_nad.pdb} 
-
-echo rosetta_scripts.linuxgccrelease @ scripts/enzdes.flags -s ${LINE/.pdb/_nad.pdb} -extra_res_fa params/cofactor/${SCAFFOLD}.params -extra_res_fa params/ligand/${LIGAND}.params -out:path:all out/enzdes/ -enzdes:cstfile cst/enzdes/${LIGAND}_${DYAD}_NAD.cst 
+rosetta_scripts.linuxgccrelease @ scripts/enzdes.flags -s out/enzdes/$S -extra_res_fa params/cofactor/$PDB.params params/ligand/$LIG.params -enzdes:cstfile cst/enzdes/${CST}_NAD.cst 
 
